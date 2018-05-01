891cef3e2c98007c2bd0517576e65a18d409e45c
Swethasree Bhattaram <sb12@illinois.edu>
2018-05-01 09:03:38 -0500

    This is MP7

--- MainActivity.java
+++ MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         load.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Log.d(TAG, "load button");
+                Log.d(TAG, "load button clicked");
                 TextView text = findViewById(R.id.dailyQuote);
                 text.setText(getSavedQuotes().toString());
             }
